40d4b91067b06c0bafe263429bfd52a1fdcb6b1a
gcr2001 <gabriel@omaximo.com.br>
2025-02-16 18:24:35 -0300

    seguindo as regras

--- main_isr_practice.c
+++ main_isr_practice.c
@@ -6,25 +6,25 @@ const int BTN_PIN_R = 28;
 
 // ERRO: cppcheck unitvar
 // ERRO: addon IsrVarVolatile
-int btn_flag;
+volatile int btn_flag;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge.
 
     // ERRO: addon IsrPrintf
-    printf("btn pressed \n");
+    //printf("btn pressed \n");
 
     // ERRO: addon IsrNoLoop
-    while (!pio_get(BTN_PIN_R)) {
-      // ERRO: addon IsrNoDelay
-      sleep_ms(1);
-    }
+    //while (!pio_get(BTN_PIN_R)) {
+        // ERRO: addon IsrNoDelay
+    //  sleep_ms(1);
+    //}
 
     // ERRO: addon IsrPrintf
-    printf("btn released \n");
+    //printf("btn released \n");
 
     // ERRO: addon IsrNoDelay
-    sleep_ms(1);
+    //sleep_ms(1);
     btn_flag = 1;
   }
 }
